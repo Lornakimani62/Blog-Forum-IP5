80a32d310032b6cce0637a4a6278d13cf8a072f1
lorna nashipae <kimanilorna@yahoo.com>
2018-11-25 20:24:05 +0300

    This commit is to debug the email feature

--- app/auth/views.py
+++ app/auth/views.py
@@ -26,12 +26,19 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data,username=form.username.data,password_hash=form.password.data)
+        user = User(email=form.email.data,username=form.username.data,password=form.password.data)
         db.session.add(user)
         db.session.commit()
 
-        mail_message("SUBSCRIPTION CONFIRMED ","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Idea Hub","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title='New Account'
     return render_template('auth/register.html',registration_form=form)
+
+# Log Out
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='secret key'
-export MAIL_USERNAME='ideahub96gmail.com'
+export MAIL_USERNAME='bloggersforum22@gmail.com'
 export MAIL_PASSWORD='0724276722'
 
 python3.6 manage.py server
