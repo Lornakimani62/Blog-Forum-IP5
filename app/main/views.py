ee847f7c09a180ca2f7b0228c7c7ce919bf0ea4f
lorna nashipae <kimanilorna@yahoo.com>
2018-11-26 10:07:08 +0300

    This commit is to denug the upload photo functionality

--- app/main/views.py
+++ app/main/views.py
@@ -28,7 +28,7 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
-    
+
 # Update profile view function
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
@@ -48,3 +48,15 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+
+# update photos view function
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
